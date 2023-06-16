f5c89c94c539c6164f0bc39395195b94dad5f8c3
yesewhareg <hanas5294@gmail.com>
2023-06-16 06:14:31 -0700

    Print list

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,9 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -20,10 +17,9 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
-
 size_t print_listint(const listint_t *h);
-listint_s *add_nodeint_end(listint_t **head, const int *n);
-void free_list(listint_t *head);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
