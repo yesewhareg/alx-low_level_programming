5181b51300cbb5f875c594d095c66b37bf1468a7
yesewhareg <hanas5294@gmail.com>
2023-06-23 12:08:14 -0700

    A library is not a luxury but one of the necessities of life

--- 0x18-dynamic_libraries/0-memset.c
+++ 0x18-dynamic_libraries/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
+#include <string.h>
 /**
- * _memset - fill a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
- * @n: number of bytes to be changed
- *
- * Return: changed array with new value for n bytes
+ *_memset - memory for b
+ *@s: string
+ *@b: char
+ *@n: intiger
+ *Return: the valu of s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i;
 
-	for (; n > 0; i++)
-	{
-		s[i] = b;
-		n--;
-	}
+	for (i = 0; i < n; i++)
+
+	s[i] = b;
+
 	return (s);
 }
