81412d1bb58121e4afcc852072b4a4177f335cbc
yesewhareg <hanas5294@gmail.com>
2023-12-28 04:20:07 -0800

    Hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -6,7 +6,7 @@
  *
  * Return: The calculated hash.
  */
-unsigned log int hash_djb2(const unsigned char *str)
+unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
